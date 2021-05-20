c34fd5ecafba514ee2fefe88d5775d298b619714
REVATHI MYLAVARAPU <revathisrie@gmail.com>
2021-05-20 00:27:59 -0700

    added local Storage and styling

--- app.js
+++ app.js
@@ -1,197 +1,203 @@
-'use strict';
-const resultsPannelUlElem = document.getElementById('itemClicks');
+'use strict'
+//-------------------------------------------------------------------
+//Global Variables
+const resultsPannelUlElem = document.getElementById('results');
+const itemImageSectionTag = document.getElementById('all_items');
+const leftItemImageTag = document.getElementById('left_item_img');
+const middleItemImageTag = document.getElementById('middle_item_img');
+const rightItemImageTag = document.getElementById('right_item_img');
+const leftItemH2Elem = document.getElementById('left_item_h2');
+const middleItemH2Elem = document.getElementById('middle_item_h2');
+const rightItemH2Elm = document.getElementById('right_item_h2');
 
-const firstImageTag = document.getElementById('img1');
-const secondImageTag = document.getElementById('img2');
-const rightImageTag = document.getElementById('img3');
-const firstPicName = document.getElementById('picNameOne');
-const secondPicName = document.getElementById('picNameTwo');
-const rightPicName = document.getElementById('picNameThree');
+let voteCounter= 0;
 
-let voteCounter = 0;
+//---------------------------------------------------------------------
+let currentLeftItem = null;
+let currentMiddleItem = null;
+let currentRightItem = null;
+let maxVotesBeforeRendering = prompt("how many clicks are you ready for before stats");
 
-
-let firstPic = null;
-let centerPic = null;
-let rightPic = null;
-
-
-function Product(name, imgPath) {
+//---------------------------------------------------------------------------
+//make a constructor function for all item images
+const Item = function(name, imgPath, votes, timesViewed){
   this.name = name;
   this.imgPath = imgPath;
-  this.votes = 0;
-  this.timesShown = 0;
+  this.votes = votes; 
+  this.timesViewed = timesViewed;
 
-  Product.allProducts.push(this)
+  Item.allItems.push(this)
 }
+Item.allItems = [];
 
+//-----------------------------------------------------------------------------------
+const makeItemChart = function(){
+  const itemChart = document.getElementById('itemChart').getContext('2d');
+  const itemData =[]; 
+  const itemViews =[]; 
+  const itemLabels= []; 
 
-Product.allProducts = [];
-
-Product.prototype.renderItem = function(name, imageTag) {
-  imageTag.src = this.imgPath;
-  name.textContent = this.name;
-}
-
-function renderThreeProducts(firstImage, secondImage,rightImage) {
-  firstImage.renderItem(firstPicName,firstImageTag );
-  secondImage.renderItem(secondPicName,secondImageTag);
-  rightImage.renderItem(rightPicName,rightImageTag );
-
-}
-
-
-function pickProducts() {
-  const firstImgIndex = Math.floor(Math.random() * Product.allProducts.length);
-
-  let secondImgIndex;
-  let rightImgIndex;
-  
-  while (secondImgIndex === undefined || secondImgIndex === firstImgIndex) {
-    secondImgIndex = Math.floor(Math.random() * Product.allProducts.length);
+  for(let item of Item.allItems){
+    itemData.push(item.votes);
+    itemViews.push(item.timesViewed);
+    itemLabels.push(item.name);
   }
-  
-  while (rightImgIndex === undefined || rightImgIndex === firstImgIndex || rightImgIndex === secondImgIndex) {
-    rightImgIndex = Math.floor(Math.random() * Product.allProducts.length);
-  }
-  firstPic = Product.allProducts[firstImgIndex];
-  centerPic = Product.allProducts[secondImgIndex];
-  rightPic = Product.allProducts[rightImgIndex];
-}
-
-function renderResults() {
-  resultsPannelUlElem.innerHTML = '';
-  const h2Elem = document.createElement('h2');
-  h2Elem.textContent = 'Products Picked';
-  resultsPannelUlElem.appendChild(h2Elem);
-
-  for (let item of Product.allProducts) {
-    const liElm = document.createElement('li');
-    liElm.textContent = `${item.name} : ${item.votes}`;
-    resultsPannelUlElem.appendChild(liElm);
-  }
-}
-
-function handleClick(e) {
-  console.log('Hello, I am Listening');
-  let thingTheyClickedOn = e.target;
-  console.log(thingTheyClickedOn, firstImageTag.children);
-
-  if (voteCounter < 25) {
-    if (thingTheyClickedOn === firstImageTag || thingTheyClickedOn === secondImageTag || thingTheyClickedOn === rightImageTag) {
-      voteCounter++;
-      console.log("we made it")
-     
-      if (thingTheyClickedOn === firstImageTag) {
-        firstPic.votes++;
-      
-      } else if (thingTheyClickedOn === secondImageTag) {
-        centerPic.votes++;
-     
-      } else if (thingTheyClickedOn === rightImageTag) {
-        rightPic.votes++;
-      }
-
-      pickProducts();
-      console.log(firstPic,centerPic,rightPic);
-      renderThreeProducts(firstPic, centerPic, rightPic);
-
-    }  
-
-  } else {
-    firstImageTag.removeEventListener('click', handleClick);
-    secondImageTag.removeEventListener('click', handleClick);
-    rightImageTag.removeEventListener('click', handleClick);
-    renderResults();
-    callChart();
-  }
-
-}
-
-// Event Listener
-firstImageTag.addEventListener('click', handleClick);
-secondImageTag.addEventListener('click', handleClick);
-rightImageTag.addEventListener('click', handleClick);
-
-// Calling the Products
-new Product('Bag', '../img/bag.jpg');
-new Product('Banana', '../img/banana.jpg');
-new Product('Bathroom','../img/bathroom.jpg');
-new Product('Boots','../img/boots.jpg');
-new Product('Breakfast', '../img/breakfast.jpg');
-new Product('Bubble Gum', '../img/bubblegum.jpg');
-new Product('Chair', '../img/chair.jpg');
-new Product('Cthulhu', '../img/cthulhu.jpg');
-new Product('Dog-Duck', '../img/dog-duck.jpg');
-new Product('Dragon', '../img/dragon.jpg');
-new Product('Pen', '../img/pen.jpg');
-new Product('Pet-Sweep', '../img/pet-sweep.jpg');
-new Product('Scissors', '../img/scissors.jpg');
-new Product('Shark', '../img/shark.jpg');
-new Product('Sweep', '../img/sweep.png');
-new Product('tauntaun', '../img/tauntaun.jpg');
-new Product('Unicorn', '../img/unicorn.jpg');
-new Product('Water Can', '../img/water-can.jpg');
-new Product('Wine Glass', '../img/wine-glass.jpg');
-
-
-// Calling the Functions
-pickProducts();
-console.log(firstPic);
-console.log(centerPic);
-console.log(rightPic);
-renderThreeProducts(firstPic, centerPic, rightPic);
-
-var ctx = document.getElementById('chart').getContext('2d');
-
-
-
-var nameArray = [];
-var voteArray = [];
-
-
-console.log(voteArray);
-function callChart(){
-  for (let item of Product.allProducts) {
-
-    nameArray.push(item.name);
-    voteArray.push(item.votes);
-    
-    
-  }
-var myChart = new Chart(ctx, {
-  
+ //---------------------------------------------------------------------------------
+  const myChart = new Chart(itemChart, {
     type: 'bar',
     data: {
-      labels: nameArray,
+        labels: itemLabels,
         datasets: [{
             label: '# of Votes',
-            data: voteArray,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            data: itemData,
+            backgroundColor: 'yellow',
+    },{
+      label: '# of Views',
+            data: itemViews,
+            backgroundColor: 'blue',
+    }]
+  },
+  options: {
+      scales: {
+        yAxes: [{
+          ticks:{
+            beginAtZero: true
+          }
         }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
-            }
-        }
+      }
     }
-});
+  });
 }
+
+//-----------------------------------------------------------------------------------
+Item.prototype.renderItem= function(h2,imageTag){
+  imageTag.src = this.imgPath;
+  h2.textContent = this.name;
+}
+
+//-------------------------------------------------------------------------------------
+
+function renderThreeItems(leftItem,middleItem, rightItem){
+  leftItem.renderItem(leftItemH2Elem, leftItemImageTag);
+  middleItem.renderItem(middleItemH2Elem, middleItemImageTag);
+  rightItem.renderItem(rightItemH2Elm, rightItemImageTag);
+}
+
+//-------------------------------------------------------------------------------------
+function pickItems(){
+  const usedItems = [];
+  usedItems.push(currentLeftItem);
+  usedItems.push(currentMiddleItem);
+  usedItems.push(currentRightItem);
+
+  while(usedItems.includes(currentLeftItem)){
+    let leftItemIndex = Math.floor(Math.random()*Item.allItems.length);
+    currentLeftItem = Item.allItems[leftItemIndex];
+  }
+  usedItems.push(currentLeftItem);
+
+  while(usedItems.includes(currentMiddleItem)){
+    let middleItemIndex = Math.floor(Math.random()*Item.allItems.length);
+    currentMiddleItem = Item.allItems[middleItemIndex];
+  }
+  usedItems.push(currentMiddleItem);
+
+  while(usedItems.includes(currentRightItem)){
+    let rightItemIndex = Math.floor(Math.random()*Item.allItems.length);
+    currentRightItem = Item.allItems[rightItemIndex];
+
+    }
+  }
+
+//-----------------------------------------------------------------------------
+function renderResults(){
+  resultsPannelUlElem.innerHTML= '';
+  const h2Elem =document.createElement('h2');
+  h2Elem.textContent = 'Item Likes';
+  resultsPannelUlElem.appendChild(h2Elem);
+
+  for(let item of Item.allItems){
+    const liElem = document.createElement('li');
+    liElem.textContent = `${item.name}: ${item.votes} votes , ${item.timesViewed} views`;
+    resultsPannelUlElem.appendChild(liElem);
+  }
+  
+  // Update local storage with view and votes.
+  // persist all items in local storage
+updateStorage();
+}
+//-------------------------------------------------------------------------------------
+//function to update storage
+ function updateStorage(){
+  localStorage.setItem('prev_items', localStorage.getItem('items')); // to see previous key value pairs.
+  const stringifiedItems = JSON.stringify(Item.allItems);
+  localStorage.setItem('items', stringifiedItems);
+}
+
+
+//--------------------------------------------------------------------------------------
+function handleClick(e){
+  let thingTheyClickedOn = e.target;
+  console.log(thingTheyClickedOn);
+  if(voteCounter<maxVotesBeforeRendering-1){
+    if (thingTheyClickedOn ===leftItemImageTag || thingTheyClickedOn === middleItemImageTag ||thingTheyClickedOn ===rightItemImageTag){
+     voteCounter++;
+     currentLeftItem.timesViewed++;
+     currentMiddleItem.timesViewed++;
+     currentRightItem.timesViewed++;
+      if (thingTheyClickedOn ===leftItemImageTag){
+      currentLeftItem.votes++;
+      }else if(thingTheyClickedOn ===middleItemImageTag){
+        currentMiddleItem.votes++;
+      }else if(thingTheyClickedOn ===rightItemImageTag){
+        currentRightItem.votes++;
+      }
+//-------------------------------------------------------------------------------------
+    pickItems();
+    renderThreeItems(currentLeftItem,currentMiddleItem,currentRightItem);
+    }else{
+      alert('you missed the item');
+    }
+  } else{
+    itemImageSectionTag.removeEventListener('click', handleClick);
+    renderResults();
+    makeItemChart();
+  }
+}
+//----------------------------------------------------------------------------------------
+itemImageSectionTag.addEventListener('click', handleClick)
+
+//-------------------------------------------------------------------------------------
+// If local storage has items then set Item.allItems as the value from local storage and don't create these items
+if(localStorage.getItem('items')) {
+  let items = JSON.parse(localStorage.getItem('items'));
+  for(let item of items) {
+    new Item(item.name, item.imgPath,item.votes,item.timesViewed);
+  }
+  console.log(Item.allItems);
+} else {
+  new Item('bag', 'img/bag.jpg');
+  new Item('banana', 'img/banana.jpg');
+  new Item('bathroom', 'img/bathroom.jpg');
+  new Item('boots', 'img/boots.jpg');
+  new Item('breakfast', 'img/breakfast.jpg');
+  new Item('bubblegum', 'img/bubblegum.jpg');
+  new Item('chair', 'img/chair.jpg');
+  new Item('cthulhu', 'img/cthulhu.jpg');
+  new Item('dog-duck', 'img/dog-duck.jpg');
+  new Item('dragon', 'img/dragon.jpg');
+  new Item('pen', 'img/pen.jpg');
+  new Item('pet-sweep', 'img/pet-sweep.jpg');
+  new Item('scissors', 'img/scissors.jpg');
+  new Item('shark', 'img/shark.jpg');
+  new Item('sweep', 'img/sweep.png');
+  new Item('tauntaun', 'img/tauntaun.jpg');
+  new Item('unicorn', 'img/unicorn.jpg');
+  new Item('water-can', 'img/water-can.jpg');
+  new Item('wine-glass', 'img/wine-glass.jpg');
+}
+
+
+//-------------------------------------------------------------------------------------
+pickItems();
+renderThreeItems(currentLeftItem, currentMiddleItem, currentRightItem)
